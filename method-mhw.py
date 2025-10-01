26e47ad77b23f18d187c82f8330df2ec1b5aa92c
Francesco Palermo <francesco.palermo@cmcc.it>
2025-10-01 16:31:08 +0200

    removed old Maps; added new draft script for method

---
+++ method-mhw.py
@@ -0,0 +1,41 @@
+# method mhw
+#
+
+import argparse
+from datetime import datetime
+
+parser = argparse.ArgumentParser(description="MHW detection and mapping")
+# parser.add_argument("--ccpimage", type=str, required=True, help="Docker image")
+parser.add_argument("--repository", type=str, required=True, default="", help="Repository path")
+parser.add_argument("--data_path", type=str, required=True, help="Path to input data")
+parser.add_argument("--outputs_path", type=str, required=True, help="Path to output data")
+parser.add_argument("--data_source", type=str, required=True, help="Dataset ID")
+parser.add_argument("--id_output_type", type=str, required=True, help="Output type (e.g., mhw_timeseries)")
+parser.add_argument("--working_domain", type=str, required=True, help="Working domain")
+parser.add_argument("--start_time", type=str, required=True, help="Start date (YYYY-MM-DD)")
+parser.add_argument("--end_time", type=str, required=True, help="End date (YYYY-MM-DD)")
+
+args = parser.parse_args()
+
+# Assign variables
+# ccpimage = args.ccpimage
+repository = args.repository
+data_path = args.data_path
+outputs_path = args.outputs_path
+data_source = args.data_source
+id_output_type = args.id_output_type
+working_domain = args.working_domain
+start_time = datetime.strptime(args.start_time, "%Y-%m-%d")
+end_time = datetime.strptime(args.end_time, "%Y-%m-%d")
+
+# Save all arguments to outputs.txt in the outputs_path directory
+with open(f"{outputs_path}/inputs.txt", "w") as f:
+    # f.write(f"ccpimage: {ccpimage}\n")
+    f.write(f"repository: {repository}\n")
+    f.write(f"data_path: {data_path}\n")
+    f.write(f"outputs_path: {outputs_path}\n")
+    f.write(f"data_source: {data_source}\n")
+    f.write(f"id_output_type: {id_output_type}\n")
+    f.write(f"working_domain: {working_domain}\n")
+    f.write(f"start_time: {start_time.strftime('%Y-%m-%d')}\n")
+    f.write(f"end_time: {end_time.strftime('%Y-%m-%d')}\n")
