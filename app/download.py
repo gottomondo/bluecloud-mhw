8c525ba7dc01d6174474a9e6cb4719dbac780363
Francesco Palermo <francesco.palermo@cmcc.it>
2025-11-01 13:25:05 +0100

    back to dwn prev

--- app/download.py
+++ app/download.py
@@ -88,11 +88,7 @@ def main():
     p.add_argument("--delay", type=int, default=5, help="Delay between retries (s)")
     args = p.parse_args()
 
-    # ds = ast.literal_eval(args.data_source)[0]
-    ds = args.data_source
-    print(type(ds), ds)
-    return
-
+    ds = ast.literal_eval(args.data_source)[0]
     data_path = args.data_path
     clim_key = args.climatology
 
